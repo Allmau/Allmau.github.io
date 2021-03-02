88e57b2824d06a4b30ce800e81c8799e33ca812a
Allan <allanchn@hotmail.com>
2021-03-01 19:23:38 -0600

    se agrega carpeta practica con un js que calcula area

---
+++ practica_JS/app.js
@@ -0,0 +1,9 @@
+let base=prompt('Digite la base');
+let altura=prompt('Digite la altura');
+
+const area=(base,altura)=>{
+    let total = base*altura;
+    alert('La base es ' + total);       
+}
+
+area(base,altura);
