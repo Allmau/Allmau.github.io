bbde04b2cf3b460af2a82e46878335e1bd78fe3e
Allan <allanchn@hotmail.com>
2021-03-04 19:07:19 -0600

    Se actualizo la pagina que calculo la base con el uso de sweetalert2

--- practica_JS/app.js
+++ practica_JS/app.js
@@ -1,9 +1,52 @@
-let base=prompt('Digite la base');
-let altura=prompt('Digite la altura');
 
-const area=(base,altura)=>{
-    let total = base*altura;
-    alert('La base es ' + total);       
-}
+let total
+let ba
+let al
 
-area(base,altura);
+document.querySelector(".first").addEventListener('click', function(){
+    Swal.fire({
+        title: 'digite la base',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        showLoaderOnConfirm: true,
+        preConfirm: (b) => {
+            ba=b
+          return b
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      })
+    
+  });
+
+  let a = document.querySelector(".second").addEventListener('click', function(){
+    Swal.fire({
+        title: 'digite la altura',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        showLoaderOnConfirm: true,
+        preConfirm: (a) => {
+            al=a
+          return a
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      })
+    
+  });
+  
+  
+document.querySelector(".third").addEventListener('click', function(){
+    Swal.fire("El Area es " + ba*al);
+  });
+
+
+
+
+
